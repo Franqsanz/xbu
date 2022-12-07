2c9ac6f0dfd87edcec1bfbfb8addf69290a75c76
franco sanchez <franco_genta@hotmail.com>
2022-12-07 11:39:11 -0300

    fix: MIME type

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   server: {
     port: 1010,
   },
+  resolve: {
+    extensions: ['.mjs'],
+  },
   build: {
     outDir: './dist',
     chunkSizeWarningLimit: 1500,
