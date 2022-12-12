078a06a25c42fc0d0702f858af65d917f10d265b
franco sanchez <franco_genta@hotmail.com>
2022-12-12 11:51:19 -0300

    fix: test SEO

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,6 +6,8 @@ import { HeadProps } from './types';
 export function MainHead({ title, description, urlImage }: HeadProps) {
   return (
     <Helmet>
+      <meta name='theme-color' content='#2de000' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <title>{title}</title>
       <meta name='description' content={description} />
       <meta name='twitter:card' content='summary_large_image' />
@@ -23,6 +25,8 @@ export function MainHead({ title, description, urlImage }: HeadProps) {
       <meta property='og:image' content={urlImage} />
       <meta property='og:image:width' content='1358' />
       <meta property='og:image:height' content='583' />
+      <link rel='canonical' href='https://xbu.netlify.app/' />
+      <link rel='icon' type='image/svg+xml' href='/logo.png' />
     </Helmet>
   );
 }
