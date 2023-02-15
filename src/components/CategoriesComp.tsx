5d4317f958e6cdfc40bd12cc29b993f6ee46c950
franco sanchez <franco_genta@hotmail.com>
2023-02-15 11:29:42 -0300

    feat: added new categories component

---
+++ src/components/CategoriesComp.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Link } from '@chakra-ui/react';
+
+import { TagComponent } from '../components/TagComponent';
+import { useAllBooks } from '../hooks/querys';
+import { CardProps } from '../components/types';
+
+export default function CategoriesComp() {
+  const { data } = useAllBooks();
+
+  let categories = new Set();
+
+  data && data.map(({ category }: CardProps) => categories.add(category));
+  const categoryLinks = Array.from(categories).sort();
+
+  function countCategory(ctry: any) {
+    return data.filter(({ category }: CardProps) => category === ctry).length;
+  }
+
+  return (
+    <>
+      {categoryLinks.map((category, index) => (
+        <Link
+          display='flex'
+          key={index}
+          as={NavLink}
+          to={`/categories/${category}`}
+          _hover={{ outline: 'none' }}
+        >
+          <TagComponent name={category} count={countCategory(category)} m='1' />
+        </Link>
+      ))}
+    </>
+  );
+}
