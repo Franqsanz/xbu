3466152ac1e511c91f261cb42a96047669370a38
franco sanchez <franco_genta@hotmail.com>
2023-02-16 20:09:13 -0300

    style: the cards were redesigned to explore

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,27 +4,20 @@ import LazyLoad from 'react-lazy-load';
 import {
   Flex,
   Box,
-  Text,
   useColorModeValue,
   Link,
-  Icon,
   Image,
+  LinkBox,
+  LinkOverlay,
 } from '@chakra-ui/react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import { CardProps } from '../types';
 import { TagComponent } from '../TagComponent';
 import { handleImageLoad } from '../../utils/utils';
 
-export function Card({
-  id,
-  title,
-  image,
-  synopsis,
-  author,
-  category,
-}: CardProps) {
-  // const borderCard = useColorModeValue('gray.200', '#28c900');
+export function Card({ id, title, image, author, category }: CardProps) {
+  const borderCard = useColorModeValue('gray.200', 'gray.600');
+  const outlineCard = useColorModeValue('black', 'white');
   let imgUI;
 
   if (typeof image === 'undefined') {
@@ -54,66 +47,62 @@ export function Card({
 
   return (
     <>
-      <Box
-        w='400px'
+      <LinkBox
+        w='300px'
         m='2'
-        rounded='30'
-        // border='1px'
-        // borderColor={borderCard}
+        rounded='lg'
+        border='1px'
+        borderColor={borderCard}
         overflow='hidden'
-        // boxShadow='lg'
+        boxShadow='lg'
         my='5'
-        position='relative'
+        pb='4'
+        tabIndex={0}
+        _focus={{ outline: `3px solid ${outlineCard}` }}
+        _hover={{
+          borderColor: 'transparent',
+          outline: `2px solid ${outlineCard}`,
+        }}
       >
-        <Flex direction='column'>
-          <Box p='7'>
-            <Link
-              as={NavLink}
-              to={`/categories/${category}`}
-              _hover={{ outline: 'none' }}
-            >
-              <TagComponent name={category} />
-            </Link>
-          </Box>
-          {imgUI}
-          <Box
-            as='h1'
-            fontSize='2xl'
-            lineHeight='8'
-            fontWeight='800'
-            px='7'
-            mb='2'
-            color='#1f9c00'
-            textTransform='uppercase'
-          >
-            {title}
-          </Box>
-          <Box px='7' textTransform='uppercase'>
-            {author}
-          </Box>
-          <Box p='7' h='310px' mb='14'>
-            <Text noOfLines={10} lineHeight='1.6'>
-              {synopsis}
-            </Text>
-          </Box>
-          <Link
-            as={NavLink}
-            to={`/book/${id}`}
-            w='full'
-            bg='#28c900'
-            py='4'
-            px='7'
-            position='absolute'
-            bottom='0'
-            _hover={{ outline: 'none', bg: '#24b300' }}
-          >
-            <Flex align='center' color='black'>
-              Más sobre el libro
-              <Icon as={FiArrowRight} ml='2' />
+        <LinkOverlay
+          as={NavLink}
+          to={`/book/${id}`}
+          tabIndex={-1}
+          _hover={{ outline: 'none' }}
+        >
+          <Flex direction='column'>
+            <Box p='4' ml='4' zIndex='0'>
+              <Link
+                as={NavLink}
+                to={`/categories/${category}`}
+                _focus={{ outline: `2px solid ${outlineCard}` }}
+                _hover={{ outline: 'none' }}
+              >
+                <TagComponent name={category} />
+              </Link>
+            </Box>
+            {imgUI}
+            <Flex direction='column' alignItems='center'>
+              <Box
+                as='h1'
+                fontSize='lg'
+                lineHeight='8'
+                fontWeight='800'
+                mx='1'
+                textAlign='center'
+                mb='2'
+                color='#1f9c00'
+                textTransform='uppercase'
+              >
+                {title}
+              </Box>
+              <Box px='7' fontSize='sm' textTransform='uppercase'>
+                {author}
+              </Box>
             </Flex>
-          </Link>
-        </Flex>
-      </Box>
+          </Flex>
+        </LinkOverlay>
+      </LinkBox>
     </>
   );
 }
