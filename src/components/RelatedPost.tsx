ee120425f2f08cc49c4affbeb9e980edd2f7c9fa
franco sanchez <franco_genta@hotmail.com>
2023-02-28 09:27:06 -0300

    fix: `ReleatedPost.tsx` now you will always see 3 books

--- src/components/RelatedPost.tsx
+++ src/components/RelatedPost.tsx
@@ -9,7 +9,17 @@ export default function RelatedPost({ currentBookId }: ReleatedBooksProps) {
   const colorCard = useColorModeValue('gray.900', 'gray.100');
   const { data, refetch } = useRelatedPost();
 
-  const relatedBooks = data.filter((book: any) => book.id !== currentBookId);
+  // Filtrar el ID que coincide con el libro que se esta viendo para evitar ver el mismo en los relacionados,
+  // si eso pasa se hace un refetch() a la api para mostrar otros libros,
+  // si no hay coincidencias no se hace una actualización.
+
+  const relatedBooks = data.filter((book: any) => {
+    if (book.id === currentBookId) {
+      refetch();
+    }
+
+    return book.id !== currentBookId;
+  });
 
   return (
     <>
