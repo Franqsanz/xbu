2a6ccf97f710d26f7604d60f632502e32ff37d0a
franco sanchez <franco_genta@hotmail.com>
2023-03-14 17:50:42 -0300

    test: compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   server: {
     port: 1010,
   },
