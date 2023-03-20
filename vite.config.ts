15e8d47fec6b44cad7db9151948c54241f2cd2cf
franco sanchez <franco_genta@hotmail.com>
2023-03-20 11:23:28 -0300

    feat: added vite plugin x2

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 // import svgr from 'vite-plugin-svgr';
 import compression from 'vite-plugin-compression';
 import webfontDownload from 'vite-plugin-webfont-dl';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    splitVendorChunkPlugin(),
     react(),
     webfontDownload(),
+    ViteMinifyPlugin({}),
     compression({
       algorithm: 'brotliCompress',
       ext: '.js,.css,.html,.svg',
