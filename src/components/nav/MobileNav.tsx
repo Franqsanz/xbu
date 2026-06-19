934dcf73c7e80818e2bc47db6c54fd19eb030599
franco sanchez <franco_genta@hotmail.com>
2026-06-19 13:25:22 -0300

    fix: lower mobile nav z-index so zoom overlay sits on top

--- src/components/nav/MobileNav.tsx
+++ src/components/nav/MobileNav.tsx
@@ -134,7 +134,7 @@ export function MobileNav() {
         backdropFilter='auto'
         backdropBlur='12px'
         direction='column'
-        zIndex='100'
+        zIndex='10'
       >
         <Flex as='nav' w='100%' justify='space-between' align='center'>
           <Flex align='center'>
